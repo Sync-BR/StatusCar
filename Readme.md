fd2e322ee6ed5546d70c914699f8859d55d7b23d
Sync <eduardofreitasfr@gmail.com>
2024-09-03 16:19:18 -0300

    Melhorado estrutura & adicionado novos metados

--- Readme.md
+++ Readme.md
@@ -11,3 +11,19 @@ Link para adicioanr usuario em json: `http://localhost:8080/api/user/add`
     "login": "joaosilva",
     "senha": "senhaSecreta123"
     }
+### Exemplo para deletar um usuario
+Link para deletar um usuario: http://localhost:8080/api/user/delete{ìd} <-Adicione o id no final do link>
+
+### Exemplo para efetuar uma atualização de um usuario
+Link para update:  http://localhost:8080/api/user/update{id}
+corpo para enviar com os dados atualizado.
+
+        {
+        "nome": "João da Silva",
+        "cpf": "12345678900",
+        "telefone": "(11) 98765-4321",
+        "email": "joao.silva@example.com",
+        "endereco": "Rua Exemplo, 123, São Paulo, SP",
+        "login": "joaosilva",
+        "senha": "senhaSecreta123"
+        }
--- src/main/java/com/oficina/oficinacarro/controller/ClienteController.java
+++ src/main/java/com/oficina/oficinacarro/controller/ClienteController.java
@@ -5,12 +5,11 @@ import com.oficina.oficinacarro.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.swing.text.html.parser.Entity;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
@@ -18,9 +17,28 @@ public class ClienteController {
     @Autowired(required = true)
     private ClienteRepository clienteRepository;
 
+    public ResponseEntity<List<ClienteModel>> returnClientes(){
+        List<ClienteModel> clientes = clienteRepository.findAll();
+        return new ResponseEntity<>(clientes, HttpStatus.OK);
+    }
+
     @PostMapping("/user/add")
-    public ResponseEntity<HttpStatus> addCliente(@RequestBody ClienteModel cliente){
+    public ResponseEntity<HttpStatus> addCliente(@RequestBody ClienteModel cliente) {
         clienteRepository.save(cliente);
         return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/user/delete{id}")
+    public ResponseEntity<HttpStatus> deleteCliente(@PathVariable Long id) {
+        clienteRepository.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+
+    }
+
+    @PutMapping("/user/update{id}")
+    public ResponseEntity<HttpStatus> updateCliente(@PathVariable Long id,@RequestBody ClienteModel cliente) {
+        cliente.setId(Integer.parseInt(id.toString()));
+        clienteRepository.save(cliente);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
